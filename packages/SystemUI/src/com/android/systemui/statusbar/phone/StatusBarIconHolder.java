6f92cdb0f9679d11f4601639ef99311a290b8a81
SagarMakhar <sagarmakhar@gmail.com>
2017-07-19 23:46:45 +0530

    SystemUI: Introduce dynamic VoLTE & VoWiFi icons
    wave edits:
    - massive code cleanups, fixes, improvements
    - add toggle to statusbar tuner
    
    SKULSHADY: Update drawables from OnePlus 8 Pro
    
    [cjh1249131356]: Adapt for A12.1
    
    neobuddy89: Clean up unrequired derps. Adapted for A13 QPR1
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Change-Id: Iad808a404bb2c42c2fb2d228644f242ac26647ce
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    
    Squashed with:
    
    Author: Alex Yustasov <alex9yust@gmail.com>
    Date:   Sun Jan 1 17:23:05 2023 +0000
    
        fixup! Improvements for dynamic VoLTE & VoWiFi icons
    
        * fixes https://github.com/crdroidandroid/issue_tracker/issues/65
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Tue Dec 27 20:09:20 2022 +0530
    
        Improvements for dynamic VoLTE & VoWiFi icons
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
@@ -23,6 +23,7 @@ import android.graphics.drawable.Icon;
 import android.os.UserHandle;
 
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.systemui.statusbar.connectivity.ImsIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.CallIndicatorIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.MobileIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.WifiIconState;
@@ -65,12 +66,15 @@ public class StatusBarIconHolder {
 
     public static final int TYPE_NETWORK_TRAFFIC = 5;
 
+    public static final int TYPE_IMS = 7;
+
     @IntDef({
             TYPE_ICON,
             TYPE_WIFI,
             TYPE_MOBILE,
             TYPE_MOBILE_NEW,
-            TYPE_WIFI_NEW
+            TYPE_WIFI_NEW,
+            TYPE_IMS
     })
     @Retention(RetentionPolicy.SOURCE)
     @interface IconType {}
@@ -78,6 +82,8 @@ public class StatusBarIconHolder {
     private StatusBarIcon mIcon;
     private WifiIconState mWifiState;
     private MobileIconState mMobileState;
+    private ImsIconState mImsState;
+
     private @IconType int mType = TYPE_ICON;
     private int mTag = 0;
 
@@ -111,6 +117,13 @@ public class StatusBarIconHolder {
         StatusBarIconHolder holder = new StatusBarIconHolder();
         holder.mIcon = new StatusBarIcon(UserHandle.SYSTEM, context.getPackageName(),
                 Icon.createWithResource( context, resId), 0, 0, contentDescription);
+        return holder;
+    }
+
+    public static StatusBarIconHolder fromImsIconState(ImsIconState state) {
+        StatusBarIconHolder holder = new StatusBarIconHolder();
+        holder.mImsState = state;
+        holder.mType = TYPE_IMS;
         return holder;
     }
 
@@ -203,6 +216,14 @@ public class StatusBarIconHolder {
         return holder;
     }
 
+    public ImsIconState getImsState() {
+        return mImsState;
+    }
+
+    public void setImsState(ImsIconState state) {
+        mImsState = state;
+    }
+
     public boolean isVisible() {
         switch (mType) {
             case TYPE_ICON:
@@ -218,6 +239,8 @@ public class StatusBarIconHolder {
                 return true;
             case TYPE_NETWORK_TRAFFIC:
                 return true;
+            case TYPE_IMS:
+                return mImsState.visible;
             default:
                 return true;
         }
@@ -245,6 +268,10 @@ public class StatusBarIconHolder {
             case TYPE_WIFI_NEW:
                 // The new pipeline controls visibilities via the view model and view binder, so
                 // ignore setVisible.
+                break;
+                
+            case TYPE_IMS:
+                mImsState.visible = visible;
                 break;
         }
     }
