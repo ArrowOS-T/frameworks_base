1553dd7cc39c67680a09ceba815f653379c796a1
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-14 07:51:36 +0800

    base: Avoid hiding development status to system apps
    test: check if developer options is exposed after enabling through build number
    
    Change-Id: I7c72ed02f484b19da2633839055c49a36b086f25
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3873,7 +3873,7 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def, int userHandle) {
             String v = getStringForUser(cr, name, userHandle);
-            if (isDevelopmentUri(name)) {
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
                 return 0;
             }
             return parseIntSettingWithDefault(v, def);
@@ -3906,7 +3906,7 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int userHandle)
                 throws SettingNotFoundException {
-            if (isDevelopmentUri(name)) {
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
                 return 0;
             }
             String v = getStringForUser(cr, name, userHandle);
@@ -6700,7 +6700,7 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def, int userHandle) {
             String v = getStringForUser(cr, name, userHandle);
-            if (isDevelopmentUri(name)) {
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
                 return 0;
             }
             return parseIntSettingWithDefault(v, def);
@@ -6733,7 +6733,7 @@ public final class Settings {
         public static int getIntForUser(ContentResolver cr, String name, int userHandle)
                 throws SettingNotFoundException {
             String v = getStringForUser(cr, name, userHandle);
-            if (isDevelopmentUri(name)) {
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
                 return 0;
             }
             return parseIntSetting(v, name);
@@ -16828,7 +16828,7 @@ public final class Settings {
          * or not a valid integer.
          */
         public static int getInt(ContentResolver cr, String name, int def) {
-            if (isDevelopmentUri(name)) {
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
                 return 0;
             }
             String v = getString(cr, name);
@@ -16855,7 +16855,7 @@ public final class Settings {
          */
         public static int getInt(ContentResolver cr, String name)
                 throws SettingNotFoundException {
-            if (isDevelopmentUri(name)) {
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
                 return 0;
             }
             String v = getString(cr, name);
@@ -19270,9 +19270,18 @@ public final class Settings {
         return packages[0];
     }
     
-    public static boolean isDevelopmentUri(@NonNull String name) {
-        return name.equals(Global.ADB_ENABLED) ||
-               name.equals(Global.ADB_WIFI_ENABLED) ||
-               name.equals(Global.DEVELOPMENT_SETTINGS_ENABLED);
+    public static boolean shouldHideDevStatus(@NonNull String packageName, @NonNull String name) {
+        return (name.equals(Settings.Global.ADB_ENABLED) ||
+               name.equals(Settings.Global.ADB_WIFI_ENABLED) ||
+               name.equals(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED)) && !isCallerSystemApp(packageName);
+    }
+
+    private static boolean isCallerSystemApp(@NonNull String packageName) {
+        if (packageName.equals("android") 
+            || packageName.startsWith("com.android.")
+            || packageName.contains("android.settings.intelligence")) {
+            return true;
+        }
+        return false;
     }
 }
