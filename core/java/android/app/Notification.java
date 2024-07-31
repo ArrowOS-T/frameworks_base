ee7874ee3a15297de952635bcbb300ff380f0406
Ioana Alexandru <aioana@google.com>
2024-07-31 13:46:30 +0000

    Check more URIs in notifications
    Bug: 281044385
    Test: presubmit + tested in current release
    
    (cherry picked from commit f47b41a138ebd60f7b518fb6a9d8aa8230488422,
    includes changes from commit 57bf60dd7b6a0a0e9785231f8ec25a458fedde64
    and commit 47fa2f79584b0a4e9ca7e9c6b237c4e5cf699032)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:986e36ce03b119253e5b7c7fd39bdd0324e0dc8a)
    Merged-In: I1ce6bebd9452466d005505dc5b99a0fdc0e05e80
    Change-Id: I1ce6bebd9452466d005505dc5b99a0fdc0e05e80

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -2850,7 +2850,7 @@ public class Notification implements Parcelable
                     Person.class);
             if (people != null && !people.isEmpty()) {
                 for (Person p : people) {
-                    visitor.accept(p.getIconUri());
+                    p.visitUris(visitor);
                 }
             }
 
@@ -2870,7 +2870,7 @@ public class Notification implements Parcelable
             // Notification Listeners might use directly (without the isStyle check).
             final Person person = extras.getParcelable(EXTRA_MESSAGING_PERSON, Person.class);
             if (person != null) {
-                visitor.accept(person.getIconUri());
+                person.visitUris(visitor);
             }
 
             final Parcelable[] messages = extras.getParcelableArray(EXTRA_MESSAGES,
@@ -2878,12 +2878,7 @@ public class Notification implements Parcelable
             if (!ArrayUtils.isEmpty(messages)) {
                 for (MessagingStyle.Message message : MessagingStyle.Message
                         .getMessagesFromBundleArray(messages)) {
-                    visitor.accept(message.getDataUri());
-
-                    Person senderPerson = message.getSenderPerson();
-                    if (senderPerson != null) {
-                        visitor.accept(senderPerson.getIconUri());
-                    }
+                    message.visitUris(visitor);
                 }
             }
 
@@ -2892,12 +2887,7 @@ public class Notification implements Parcelable
             if (!ArrayUtils.isEmpty(historic)) {
                 for (MessagingStyle.Message message : MessagingStyle.Message
                         .getMessagesFromBundleArray(historic)) {
-                    visitor.accept(message.getDataUri());
-
-                    Person senderPerson = message.getSenderPerson();
-                    if (senderPerson != null) {
-                        visitor.accept(senderPerson.getIconUri());
-                    }
+                    message.visitUris(visitor);
                 }
             }
 
@@ -2906,7 +2896,7 @@ public class Notification implements Parcelable
             // Extras for CallStyle (same reason for visiting without checking isStyle).
             Person callPerson = extras.getParcelable(EXTRA_CALL_PERSON, Person.class);
             if (callPerson != null) {
-                visitor.accept(callPerson.getIconUri());
+                callPerson.visitUris(visitor);
             }
             visitIconUri(visitor, extras.getParcelable(EXTRA_VERIFICATION_ICON, Icon.class));
         }
@@ -8781,6 +8771,18 @@ public class Notification implements Parcelable
                     bundles[i] = messages.get(i).toBundle();
                 }
                 return bundles;
+            }
+
+            /**
+             * See {@link Notification#visitUris(Consumer)}.
+             *
+             * @hide
+             */
+            public void visitUris(@NonNull Consumer<Uri> visitor) {
+                visitor.accept(getDataUri());
+                if (mSender != null) {
+                    mSender.visitUris(visitor);
+                }
             }
 
             /**
