69dbbdfbb5fc918dfd7493785a903cd1a63b2514
Iverz <asterixiverz@gmail.com>
2024-10-20 12:19:23 +0000

    Revert "SystemUI: Hide wifi airplane spacer when vowifi is available"
    This reverts commit 25b63189495ff4ee0bd0abc66fe6bd81a226fbbd.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -212,14 +212,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
             updateShowWifiSignalSpacer(newState);
         }
         newState.slot = mSlotWifi;
-        // Show the airplane spacer only if the mobile state is null
-        // or vowifi is not visible.
-        // Otherwise wifi icon view will end up with two spacers
-        // (signal and airplane).
-        // so it creates large space between wifi and VoWifi icon.
-        MobileIconState first = getFirstMobileState();
-        newState.airplaneSpacerVisible = mIsAirplaneMode &&
-                (first == null || first.typeId != R.drawable.ic_vowifi);
+        newState.airplaneSpacerVisible = mIsAirplaneMode;
         updateWifiIconWithState(newState);
         mWifiIconState = newState;
     }
