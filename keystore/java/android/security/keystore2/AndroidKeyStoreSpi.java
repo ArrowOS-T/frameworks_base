aa14e74899f672c5df6229e2e69aacb43fd5271b
Praditia Nur <pnplusplus@gmail.com>
2024-11-06 13:34:25 +0100

    core: Import PropImitationHooks [SQUASHED]
    Imported from https://github.com/KusumaOS/android_frameworks_base.
    Also improvement some code to easier for use.
    
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -43,6 +43,7 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.PropImitationHooks;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -167,6 +168,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        PropImitationHooks.onEngineGetCertificateChain();
 
         KeyEntryResponse response = getKeyMetadata(alias);
 
